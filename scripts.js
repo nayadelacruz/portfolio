66441202e58885cd508d97817bed8ffbf04d5600
Nayade De La Cruz <nayadelacruz@gmail.com>
2026-08-11 13:48:42 -0700

    initial portfolio website

---
+++ scripts.js
@@ -0,0 +1,15 @@
+// Smooth scrolling for navigation links
+document.querySelectorAll('nav a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href');
+        const targetSection = document.querySelector(targetId);
+
+        if (targetSection) {
+            targetSection.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        }
+    });
+});
